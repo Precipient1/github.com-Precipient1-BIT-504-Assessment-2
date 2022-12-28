0548cde6d0732517ac8ed65a2b829540274d90a6
Shan Merito <shannonmerito@myop.ac.nz>
2022-12-28 20:40:25 +1300

    Full code modification

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-  // Add your JavaScript here
-  const bookTableBtn = document.querySelector('#book-table-btn');
-  const menusBtn = document.querySelector('#menus-btn');
-  const aboutBtn = document.querySelector('#about-btn');
-  const hoursBtn = document.querySelector('#hours-btn');
-  const homePage = document.querySelector('.home-page');
-  const reservationPage = document.querySelector('.reservation-page');
-  const aboutPage = document.querySelector('.about-page');
-  const hoursPage = document.querySelector('.hours-page');
+  // Form validation
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const phone = form.elements.phone.value;
+  const date = form.elements.date.value;
+  const time = form.elements.time.value;
+  if (!name || !email || !phone || !date || !time) {
+    alert('All fields are required');
+    return;
+  }
+  alert(`Reservation made: ${name}, ${email}, ${phone}, ${date}, ${time}`);
+  form.reset();
+});
 
-  bookTableBtn.addEventListener('click', () => {
-    homePage.style.display = 'none';
-    reservationPage.style.display = 'block';
-    aboutPage.style.display = 'none';
-    hoursPage.style.display = 'none';
-  });
-
-  menusBtn.addEventListener('click', () => {
-    homePage.style.display = 'block';
-    reservationPage.style.display = 'none';
-    aboutPage.style.display = 'none';
-    hoursPage.style.display = 'none';
-  });
-
-  aboutBtn.addEventListener('click', () => {
-    homePage.style.display = 'none';
-    reservationPage.style.display = 'none';
-    aboutPage.style.display = 'block';
-    hoursPage.style.display = 'none';
-  });
-
-  hoursBtn.addEventListener('click', () => {
-    homePage.style.display = 'none';
-    reservationPage.style.display = 'none';
-    aboutPage.style.display = 'none';
-    hoursPage.style.display = 'block';
-  });
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-      nav.classList.add('visible');
-    } else {
-      nav.classList.remove('visible');
-    }
-  });
-  
+// Google Maps API
+const map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+});
