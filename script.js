83f1e2d5129ea6d91372a691fdba49599f97a0f8
Shan Merito <shannonmerito@myop.ac.nz>
2023-01-15 13:20:57 +1300

    New File Additions, file mod updates

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+
+// Get the carousel element
+const carousel = document.getElementById("bg-carousel");
+
+// Next button
+const nextButton = document.getElementById("next-button");
+nextButton.addEventListener("click", () => {
+  carousel.carousel("next");
+});
+
+// Prev button
+const prevButton = document.getElementById("prev-button");
+prevButton.addEventListener("click", () => {
+  carousel.carousel("prev");
+});
+
+// Pause button
+const pauseButton = document.getElementById("pause-button");
+pauseButton.addEventListener("click", () => {
+  carousel.carousel("pause");
+});
+
+// Play button
+const playButton = document.getElementById("play-button");
+playButton.addEventListener("click", () => {
+  carousel.carousel("cycle");
+});
+
 // form validation
 const form = document.getElementById("reservationForm");
 form.addEventListener("submit", (e) => {
@@ -9,11 +38,14 @@ form.addEventListener("submit", (e) => {
   const time = document.getElementById("time").value;
   const guests = document.getElementById("guests").value;
 
-  if (name === "" || email === "" || phone === "" || date === "" || time === "" || guests === "") {
-    alert("Please fill out all required fields.");
-  } else {
+  if (name === "" || email === "" || phone === "" || date === "" || time === "" || guests === "") 
+    {
+        alert("Please fill out all required fields.");
+    } 
+    else 
+    {
     form.submit();
     alert("Reservation submitted successfully!");
-  }
+    }
 }
 );
